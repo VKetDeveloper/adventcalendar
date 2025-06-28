180e661b0036c816c7dc112d450541b744abac43
Serie <120567699+RearAlice@users.noreply.github.com>
2025-06-28 14:01:24 +0900

    Refactor Footer component and remove unused API route
    Moved Footer.css to the components directory and updated the import path in Footer.tsx. Fixed className typos in Footer.tsx. Removed the unused hello.ts API route.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,26 +2,26 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import '../styles/Footer.css'; // Assuming you have a CSS file for styling
+import './Footer.css'; // Assuming you have a CSS file for styling
 
 const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="footer">
-      <div className="md-flex md-justify-between">
- <Link href="../" className="flex items-center">
-                            <Image
-                                src="/img/logo.png"
-                                alt="VRUGD Logo"
-                                width={120}
-                                height={40}
-                                className="h-auto"
-                            />
-                        </Link>
+      <div className="md:flex md:justify-between">
+        <Link href="../" className="flex items-center">
+          <Image
+            src="/img/logo.png"
+            alt="VRUGD Logo"
+            width={120}
+            height={40}
+            className="h-auto"
+          />
+        </Link>
       </div>
       <hr />
       <p className="copyright">
-Copyright © {currentYear} VketReal Unofficial Garage Developers. All Rights Reserved.
+        Copyright © {currentYear} VketReal Unofficial Garage Developers. All Rights Reserved.
       </p>
     </footer>
   );
